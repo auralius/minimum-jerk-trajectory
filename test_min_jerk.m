a6be9ca6c10e25a7644cb16b1a8c05f8ad3e9a02
Auralius Manurung <manurung.auralius@gmail.com>
2021-03-02 19:25:08 +0700

    fixing typos, commenting, and prove a figure for the exmple

--- test_min_jerk.m
+++ test_min_jerk.m
@@ -1,19 +1,27 @@
-i = 1;
+close all
+clear all
+
+% create time vector
 t = 0:0.01:0.5;
 
+% generate the minimum jerk trajectory
 out = min_jerk([5 0 0], [10 10 5], t);
 
+% plot the results
 subplot(3,1,1);
 plot(t, out(:,1));
 xlabel('t');
-ylabel('x');
+ylabel('x(t)');
 
 subplot(3,1,2);
 plot(t, out(:,2));
 xlabel('t');
-ylabel('y');
+ylabel('y(t)');
 
 subplot(3,1,3);
 plot(t, out(:,3));
 xlabel('t');
-ylabel('z');
+ylabel('z(t)');
+
+set(findall(gcf,'-property','FontSize'),'FontSize',12)
+set(findall(gcf,'-property','FontName'),'FontName','Times New Roman')
